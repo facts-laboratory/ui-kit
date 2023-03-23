e8fc8e908eab6826c73b2880e738dc5b9fc718be
Justin Shaw <mogulx_operates@yahoo.com>
2023-03-23 08:52:07 -0500

    fix: export assets / layout from ui-kit

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -1,3 +1,9 @@
-export * from './lib/content-container/content-container';
-export * from './lib/footer/footer';
-export * from './lib/header/header';
+import { StandardContentContainer } from './lib/content-container/content-container';
+import { StandardFooter } from './lib/footer/footer';
+import { StandardHeader } from './lib/header/header';
+export * as assets from '@facts-kit/assets';
+export const layout = {
+  StandardHeader,
+  StandardContentContainer,
+  StandardFooter,
+};
